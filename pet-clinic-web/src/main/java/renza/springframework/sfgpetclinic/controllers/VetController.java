47a9c15994ccb8e3cef03d3b09c2bf1611366ae3
renza <alexandrerenza@gmail.com>
2019-09-14 16:33:51 -0300

    List the Vet on the Index Page. Closes #15.

--- pet-clinic-web/src/main/java/renza/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/renza/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,14 +1,24 @@
 package renza.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import renza.springframework.sfgpetclinic.services.VetService;
 
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets", "vets/index", "vets/index.html"})
-    public String listVets(){
+    private final VetService vetService;
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
+    @RequestMapping({"/vets", "vets/index", "vets/index.html"})
+    public String listVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 
