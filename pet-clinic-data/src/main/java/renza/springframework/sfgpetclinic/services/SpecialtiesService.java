a07683939fb03017d100c12174cdaa30799fd566
renza <alexandrerenza@gmail.com>
2019-09-18 17:10:46 -0300

    Create Specialty Map Service. Closes #30.

---
+++ pet-clinic-data/src/main/java/renza/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package renza.springframework.sfgpetclinic.services;
+
+import renza.springframework.sfgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
