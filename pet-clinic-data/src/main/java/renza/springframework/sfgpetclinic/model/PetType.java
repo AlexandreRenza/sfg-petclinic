e29538d2b1dbdd657293de66a9f2e2ad095e8884
renza <alexandrerenza@gmail.com>
2019-09-26 00:03:08 -0300

    Implemented map jpa on owner, pet, petType. Closes #34.

--- pet-clinic-data/src/main/java/renza/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/renza/springframework/sfgpetclinic/model/PetType.java
@@ -1,8 +1,18 @@
 package renza.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="types")
 public class PetType extends BaseEntity{
 
+    @Column(name= "name")
     private String name ;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "petType")
+    private Set<Pet> pets = new HashSet<>();
 
     public String getName() {
         return name;
@@ -10,5 +20,13 @@ public class PetType extends BaseEntity{
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
     }
 }
