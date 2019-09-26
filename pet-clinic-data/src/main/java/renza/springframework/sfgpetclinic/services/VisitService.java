9d17a1c1a7a1419c9f30106ae170062cd5bb6672
renza <alexandrerenza@gmail.com>
2019-09-26 19:18:11 -0300

    Implemented Visit Map Service. Closes #43.

---
+++ pet-clinic-data/src/main/java/renza/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,8 @@
+package renza.springframework.sfgpetclinic.services;
+
+import renza.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+
+}
