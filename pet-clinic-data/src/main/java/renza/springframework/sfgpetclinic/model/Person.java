af2f4a22e0ea11136e022c66da2c656e73924b1f
AlexandreRenza <alexandrerenza@gmail.com>
2019-11-12 16:08:03 -0500

    Test Owner Controller. Closes #50

--- pet-clinic-data/src/main/java/renza/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/renza/springframework/sfgpetclinic/model/Person.java
@@ -8,12 +8,15 @@ import lombok.Setter;
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
+/**
+ * Created by jt on 7/13/18.
+ */
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @MappedSuperclass
-public class Person extends BaseEntity{
+public class Person extends BaseEntity {
 
     public Person(Long id, String firstName, String lastName) {
         super(id);
@@ -23,7 +26,10 @@ public class Person extends BaseEntity{
 
     @Column(name = "first_name")
     private String firstName;
-    @Column(name="last_name")
+
+    @Column(name = "last_name")
     private String lastName;
 
 }
+
+
--- pet-clinic-data/src/main/java/renza/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/renza/springframework/sfgpetclinic/model/Vet.java
@@ -15,10 +15,14 @@ import java.util.Set;
 @Table(name="vets")
 public class Vet extends Person{
 
+
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name="vet_specialties",
                joinColumns = @JoinColumn(name = "vet_id"),
                inverseJoinColumns = @JoinColumn(name="specialty_id"))
     private Set<Specialty> specialities = new HashSet<>();
 
+    public Vet(Long id, String firstName, String lastName) {
+        super(id, firstName, lastName);
+    }
 }
